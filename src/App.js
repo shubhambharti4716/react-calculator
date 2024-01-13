04b03e1aefb8146e263e5208069eda2f0853c194
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2024-01-13 19:32:14 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,24 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+
+  const [num1, setNum1] = useState("");
+  const [num2, setNum2] = useState("");
+
+  const [status, setStatus] = useState("");
+  const [message, setMessage] = useState("");
+  const [result, setResult] = useState(null);
+
+  function calculate(type) {
+
+    const number1 = parseFloat(num1);
+    const number2 = parseFloat(num2);
+
+    if (isNaN(number1)) {
+      setStatus("Error!");
+      setMessage("Num 1 Cannot Be Empty");
+      setResult(null);
+      return;
+    }
+    if (isNaN(number2)) {
+      setStatus("Error!");
+      setMessage("Num 2 Cannot Be Empty");
+      setResult(null);
+      return;
+    }
+
+    switch (type) {
+      case "+":
+        setResult(number1 + number2);
+        break;
+      case "-":
+        setResult(number1 - number2);
+        break;
+      case "*":
+        setResult(number1 * number2);
+        break;
+      case "/":
+        setResult(number1 / number2);
+        break;
+      default:
+        return;
+    }
+    setStatus("Success!");
+    setMessage("");
+
+    // resetting input values to empty
+    setNum1("");
+    setNum2("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="main">
+        <div className="calculator-container">
+        <h1>React Calculator</h1>
+        <input
+          type="number"
+          placeholder="Num 1"
+          onChange={(e) => setNum1(e.target.value)}
+          value={num1}
+        />
+        <input
+          type="number"
+          placeholder="Num 2"
+          onChange={(e) => setNum2(e.target.value)}
+          value={num2}
+        />
+        <div className="buttons-container">
+          <button
+            type="button"
+            className="button"
+            onClick={() => calculate("+")}
+          >
+            +
+          </button>
+          <button
+            type="button"
+            className="button"
+            onClick={() => calculate("-")}
+          >
+            -
+          </button>
+          <button
+            type="button"
+            className="button"
+            onClick={() => calculate("*")}
+          >
+            *
+          </button>
+          <button
+            type="button"
+            className="button"
+            onClick={() => calculate("/")}
+          >
+            /
+          </button>
+        </div>
+        <div className="message-container">
+          {status && (
+            <div>
+              {status === "Error!" ? (<p className="error">{status}</p>) : (<p className="success">{status}</p>)}
+              {message  ? (<p>{message}</p>) : (<p>Result = {result}</p>)}
+            </div>
+          )}
+        </div>
+      </div>
+      </div>
   );
 }
 
