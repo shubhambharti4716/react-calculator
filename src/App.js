b4b034aa960013c240f7aecb40622e2e90b4211b
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2024-01-13 20:35:38 +0530

    final

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-
+   // State variables to store input values, status, message, and result
   const [num1, setNum1] = useState("");
   const [num2, setNum2] = useState("");
-
   const [status, setStatus] = useState("");
   const [message, setMessage] = useState("");
   const [result, setResult] = useState(null);
-
+  
+  // Function to validate input values
   function validateInputs() {
+    // Check if either of the input fields is empty
     if (!num1.trim() || !num2.trim()) {
       setStatus("Error!");
       setMessage("Please enter both numbers.");
@@ -18,6 +19,7 @@ function App() {
       return false;
     }
 
+     // Regular expression to check if the input values are valid numbers
     const regex = /^-?\d*\.?\d+$/;
     if (!regex.test(num1) || !regex.test(num2)) {
       setStatus("Error!");
@@ -25,15 +27,16 @@ function App() {
       setResult(null);
       return false;
     }
-
     return true;
   }
 
+  // Function to perform the calculation based on the operation type
   function calculate(type) {
     if (validateInputs()) {
     const number1 = parseFloat(num1);
     const number2 = parseFloat(num2);
 
+    // Check for division by zero
     if (type === "/" && number2 === 0) {
       setStatus("Error!");
       setMessage("Cannot divide by zero.");
@@ -41,6 +44,7 @@ function App() {
       return;
     }
 
+    // Perform the calculation based on the operation type
     switch (type) {
       case "+":
         setResult(number1 + number2);
@@ -57,10 +61,9 @@ function App() {
       default:
         return;
     }
+     // Set status to Success, clear message, and reset input values
     setStatus("Success!");
     setMessage("");
-
-    // resetting input values to empty
     setNum1("");
     setNum2("");
   }
@@ -70,18 +73,20 @@ function App() {
       <div className="main">
         <div className="calculator-container">
         <h1>React Calculator</h1>
+        {/* Input fields for entering numbers */}
         <input
-          type="number"
+          type="text"
           placeholder="Num 1"
           onChange={(e) => setNum1(e.target.value)}
           value={num1}
         />
         <input
-          type="number"
+          type="text"
           placeholder="Num 2"
           onChange={(e) => setNum2(e.target.value)}
           value={num2}
         />
+        {/* Buttons for different operations */}
         <div className="buttons-container">
           <button
             type="button"
@@ -112,6 +117,7 @@ function App() {
             /
           </button>
         </div>
+        {/* Display status, error/success message, and result */}
         <div className="message-container">
           {status && (
             <div>
